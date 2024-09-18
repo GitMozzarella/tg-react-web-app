2ac371c006549882790f40688e3c9bf4bd60768a
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-09-19 01:23:51 +0500

    test: routing

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ export const App = () => {
 			<Header />
 			<Routes>
 				<Route index element={<Home />} />
-				<Route path={'/yandexmap'} element={<YandexMap />} />
+				<Route path={'yandexmap'} element={<YandexMap />} />
 			</Routes>
 		</div>
 	)
