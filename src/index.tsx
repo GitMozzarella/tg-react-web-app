fccf4cfc62021fc1e66107d8833ed70b52527979
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-09-19 01:14:08 +0500

    test: routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 const rootElement = document.getElementById('root')
 if (rootElement) {
 	const root = ReactDOM.createRoot(rootElement as HTMLElement)
 
 	root.render(
-		<HashRouter>
+		<BrowserRouter>
 			<React.StrictMode>
 				<App />
 			</React.StrictMode>
-		</HashRouter>
+		</BrowserRouter>
 	)
 } else {
 	console.error('Root element not found')
