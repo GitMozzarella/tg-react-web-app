2629dfa3e0dba6272b290015d45a52687b79787c
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-09-19 03:12:05 +0500

    chore: temporary

--- src/pages/YandexMap/YandexMap.tsx
+++ src/pages/YandexMap/YandexMap.tsx
@@ -1,41 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { initYMaps } from './../../lib/ymaps'
-export const YandexMap: React.FC = () => {
-	const [mapContainer, setMapContainer] = useState<HTMLElement | null>(null)
-	const [YMapComponents, setYMapComponents] = useState<any>(null)
+import './map.css'
 
-	useEffect(() => {
-		const loadMap = async () => {
-			try {
-				const { YMap, YMapDefaultSchemeLayer } = await initYMaps()
-				setYMapComponents({ YMap, YMapDefaultSchemeLayer })
-			} catch (error) {
-				console.error('Ошибка инициализации карты:', error)
-			}
-		}
-		loadMap()
-	}, [])
-
-	useEffect(() => {
-		if (mapContainer && YMapComponents) {
-			const { YMap, YMapDefaultSchemeLayer } = YMapComponents
-			const map = new YMap(mapContainer, {
-				location: {
-					center: [55.751244, 37.618423],
-					zoom: 10
-				}
-			})
-
-			const layer = new YMapDefaultSchemeLayer()
-			map.addChild(layer)
-		}
-	}, [mapContainer, YMapComponents])
-
-	return (
-		<div
-			id='map'
-			ref={setMapContainer}
-			style={{ width: '100%', height: '400px' }}
-		/>
-	)
+export const YandexMap = () => {
+	return <div>map</div>
 }
