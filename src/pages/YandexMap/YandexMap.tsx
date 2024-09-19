2e48620ad7c7239d537f919f06717640bcdfed4e
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-09-19 05:04:40 +0500

    feat: add ymaps

--- src/pages/YandexMap/YandexMap.tsx
+++ src/pages/YandexMap/YandexMap.tsx
@@ -1,38 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import { initYMaps } from './../../lib/ymaps'
 import './map.css'
+import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps'
 
 export const YandexMap = () => {
-	const [mapContainer, setMapContainer] = useState<HTMLElement | null>(null)
-
-	useEffect(() => {
-		async function initMap() {
-			try {
-				console.log('Начинаю инициализацию карты') // Лог
-				const { YMap, YMapDefaultSchemeLayer } = await initYMaps() // Инициализация карты
-				console.log('Компоненты карты загружены', YMap, YMapDefaultSchemeLayer) // Лог
-
-				const map = new YMap(mapContainer as HTMLElement, {
-					location: {
-						center: [55.751244, 37.618423], // Координаты центра карты (Москва)
-						zoom: 9
-					}
-				})
-				console.log('Карта создана', map) // Лог
-
-				map.addChild(new YMapDefaultSchemeLayer({}))
-			} catch (error) {
-				console.error('Ошибка инициализации карты:', error)
-			}
-		}
-
-		if (mapContainer) {
-			console.log('Контейнер карты готов') // Лог
-			initMap() // Запуск инициализации только после готовности контейнера
-		} else {
-			console.log('Контейнер карты не готов') // Лог
-		}
-	}, [mapContainer])
-
-	return <div id='map' ref={setMapContainer} />
+	return (
+		<YMaps>
+			<Map
+				defaultState={{
+					center: [55.75, 37.57],
+					zoom: 9,
+					controls: ['zoomControl', 'fullscreenControl']
+				}}
+				modules={['control.ZoomControl', 'control.FullscreenControl']}
+			>
+				<Placemark
+					modules={['geoObject.addon.balloon']}
+					defaultGeometry={[55.75, 37.57]}
+					properties={{
+						balloonContentBody:
+							'This is balloon loaded by the Yandex.Maps API module system'
+					}}
+				/>
+			</Map>
+		</YMaps>
+	)
 }
