c7f792c5e32a407482cd6fca48ed5ea72799cab6
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-09-20 15:05:31 +0500

    test: пробую перейти на версию 2.1 и добавить кластеры

--- src/pages/YandexMap/YandexMap.tsx
+++ src/pages/YandexMap/YandexMap.tsx
@@ -1,55 +1,31 @@
-// @ts-nocheck
 import { useEffect } from 'react'
 
 export const YandexMap = () => {
 	useEffect(() => {
 		const loadMap = () => {
-			// Инициализация карты
-			const map = new window.ymaps.Map('map', {
-				center: [55.751574, 37.573856], // Москва
+			const map: any = new window.ymaps.Map('map', {
+				center: [55.751574, 37.573856],
 				zoom: 9
 			})
 
-			// Используем any, чтобы избежать проблем с типами
 			const clusterer: any = new window.ymaps.Clusterer({
-				preset: 'islands#invertedVioletClusterIcons',
-				groupByCoordinates: false,
-				clusterDisableClickZoom: false,
-				clusterOpenBalloonOnClick: true,
-				clusterBalloonContentLayout: 'cluster#balloonCarousel',
-				clusterIconLayout: 'default#pieChart',
-				clusterIconPieChartRadius: 30,
-				clusterIconPieChartCoreRadius: 10,
-				clusterIconPieChartStrokeWidth: 3
+				preset: 'islands#invertedVioletClusterIcons'
 			})
 
-			// Массив с координатами для меток
 			const points = [
 				[55.751574, 37.573856],
-				[55.761574, 37.573856],
-				[55.771574, 37.583856],
-				[55.781574, 37.593856]
+				[55.761574, 37.573856]
 			]
 
-			// Создание меток на основе массива координат
 			const geoObjects = points.map(
 				point =>
-					new window.ymaps.Placemark(
-						point,
-						{
-							balloonContentHeader: 'Заголовок',
-							balloonContentBody: 'Описание метки'
-						},
-						{
-							preset: 'islands#icon',
-							iconColor: '#3b5998'
-						}
-					)
+					new window.ymaps.Placemark(point, {
+						balloonContentHeader: 'Метка',
+						balloonContentBody: 'Описание метки'
+					})
 			)
 
-			// Добавление меток в кластеризатор
 			clusterer.add(geoObjects)
-			// Добавление кластеризатора на карту
 			map.geoObjects.add(clusterer)
 		}
 
